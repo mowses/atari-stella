412e7fb95024bcdebbcf4420ad31370598364a2c
Museeeii <museeeii@gmail.com>
2021-04-27 17:19:28 -0300

    probably the most close to final prod code made

--- web/socket.php
+++ web/socket.php
@@ -1,17 +1,48 @@
 <?php
 
 error_reporting(0);
-set_time_limit(10);
+set_time_limit(60);
 
-define('IP', '0.0.0.0');
+// UDP server socket definition
+define('UDP_IP', 'localhost');
+define('UDP_PORT', 3002);
+define('UDP_BUFFER', 512);
+
+// client socket connection definition
+// clients should connect here!
+define('IP', 'localhost');
 define('PORT', '5000');
 define('MAX_CLIENT', 10);
 
 require_once 'lib/socket.php';
 
+if(!($sock = socket_create(AF_INET, SOCK_DGRAM, 0)))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+
+    die("Couldn't create socket: [$errorcode] $errormsg \n");
+}
+
+// Bind the source address
+if( !socket_bind($sock, UDP_IP, UDP_PORT) )
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+
+    die("Could not bind socket : [$errorcode] $errormsg \n");
+}
+
 $socket = new socket();
 $socket->run();
-while (TRUE){
+
+echo "Waiting for UDP data and client connections... \n";
+
+while($buf = socket_read($sock, UDP_BUFFER))
+{
     $socket->accept_multi_client();
-    $socket->speak();
+    $socket->broadcast_to_clients($buf);
 }
+
+socket_close($sock);
+socket_close($socket::$sock);
