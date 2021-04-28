439142ea42ba3a4276ee60094a4b12d9937925e2
Museeeii <museeeii@gmail.com>
2021-04-27 23:19:06 -0300

    streaming ok

--- web/php-audio.php
+++ web/php-audio.php
@@ -1,15 +1,19 @@
 <?php
-// cd /home/unknown/stella/web/; php -S localhost:2001;
-// <audio controls>
-  //   <source src="http://localhost:2001/php-audio.php" type="audio/mp3">
-  // </audio>
 
-// $file = '/var/www/html/stella/web/Kamen_Rider_Black_RX_OP.mp3';
+/*
+$file = '/var/www/html/stella/web/Kamen_Rider_Black_RX_OP.mp3';
 
-// header('Content-Type: audio/mpeg');
-// header('Content-length: ' . filesize($file));
-// print file_get_contents($file);
+header('Content-Type: audio/mpeg');
+header('Content-length: ' . filesize($file));
+print file_get_contents($file);
+*/
 
+// WE HAVE A BUG HERE
+// SINCE WE ARE RECEIVING THE DATA AS STREAM
+// A CLIENT THAT IS CONNECTING AFTER THE SESSION BEGIN
+// COULD NOT RECEIVE THE "HEADER" OF THE AUDIO
+// MAKING THE AUDIO INVALID OR CORRUPT
+// 
 $fp = fsockopen('localhost', 5000) or exit('cant connect');
 
 header('HTTP/1.1 200 OK');
