36ffe32b1a571aafee7b2a25754c198e4cd59b58
Museeeii <museeeii@gmail.com>
2021-05-03 23:22:08 -0300

    added stream header and buffer management

--- web/php-audio.php
+++ web/php-audio.php
@@ -1,4 +1,22 @@
 <?php
+// setup for streaming data
+error_reporting(0);
+if (function_exists('apache_setenv')) {
+    @apache_setenv('no-gzip', 1);
+}
+@ini_set('implicit_flush', true);
+@ini_set('output_buffering', 'off');
+@ini_set('zlib.output_compression', false);
+while (@ob_end_flush());
+ob_implicit_flush(true);
+
+// Explicitly disable caching so Varnish and other upstreams won't cache.
+header("Cache-Control: no-cache, must-revalidate");
+// Setting this header instructs Nginx to disable fastcgi_buffering and disable
+// gzip for this request.
+header('X-Accel-Buffering: no');
+///////////////////////////////////
+
 
 define('HOST', '127.0.0.1');
 define('PORT', 5000);
@@ -14,8 +32,8 @@ print file_get_contents($file);
 // $fp = fsockopen(HOST, PORT) or exit('cant connect');
 
 header('HTTP/1.1 200 OK');
-header('Content-Type: audio/webm');
-header('Cache-Control: no-cache');
+//header('Content-Type: text/html');
+header('Content-Type: audio/mp3');
 header('Content-Transfer-Encoding: chunked');
 
 // while (!feof($fp)) {
@@ -26,7 +44,7 @@ header('Content-Transfer-Encoding: chunked');
 
 //echo shell_exec('nc 127.0.0.1 5000 &');
 
-$cmd = 'ffmpeg -vn -loglevel quiet -y -i tcp://' . HOST . ':' . PORT . ' -ac 1 -b:a 8k -f webm pipe: < /dev/null 2> /dev/null';
+$cmd = 'ffmpeg -vn -loglevel quiet -y -i tcp://' . HOST . ':' . PORT . ' -ac 1 -b:a 8k -f mp3 pipe: < /dev/null 2> /dev/null';
 
 $descriptorspec = [
    ['pipe', 'r'],   // stdin is a pipe that the child will read from
@@ -39,7 +57,8 @@ $process = proc_open($cmd, $descriptorspec, $pipes);
 if (is_resource($process)) {
     while ($s = fgets($pipes[1])) {
         echo $s;
-        flush();
+        @ob_flush();
+        @flush();
     }
 }
 
