82dd8b23cb49db7ff23af152128cf5fd7383d7a7
Museeeii <museeeii@gmail.com>
2021-05-11 16:33:25 -0300

    better quality screen

--- web/game/main.js
+++ web/game/main.js
@@ -1,11 +1,17 @@
+const NTSC_screen = {
+    width: 160,
+    height: 228,
+};
+const scale = { x: 4, y: 2};
+const dpr = 2;
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d', { alpha: false });
-const pixels_image = ctx.getImageData(0, 0, 160, 228);
+const pixels_image = ctx.getImageData(0, 0, NTSC_screen.width * dpr, NTSC_screen.height * dpr);
 
 const config = {
     type: Phaser.CANVAS,
-    width: 400,
-    height: 300,
+    width: NTSC_screen.width * scale.x,
+    height: NTSC_screen.height * scale.y,
     //backgroundColor: '#ff00ff',
     scene: {
         preload: preload,
@@ -37,7 +43,7 @@ function preload ()
     this.load.script('./PlayerKeysEnum');
     this.load.script('./colors/ntsc');
     this.load.script('./mixingTable/mixingTable');
-    //this.load.script('./test/test-output-data');
+    // this.load.script('./test/test-output-data');
 }
 
 function create ()
@@ -48,10 +54,19 @@ function create ()
     player.keys.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     player.keys.fire = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.CTRL);
     
-    canvas.width = this.game.config.width;
-    canvas.height = this.game.config.height;
+    let scaleX = scale.x / dpr;
+    let scaleY = scale.y / dpr;
+    canvas.width = this.game.config.width * dpr;
+    canvas.height = this.game.config.height * dpr;
     this.textures.addCanvas('TIA_VIEW', canvas);
-    this.add.image(canvas.width / 2, canvas.height / 2, 'TIA_VIEW');
+    let image = this.add.image(canvas.width / 2 * scaleX, canvas.height / 2 * scaleY, 'TIA_VIEW');
+    image.scaleX = scaleX;
+    image.scaleY = scaleY;
+
+    setAlpha();
+
+    // gamedata.video = JSON.parse('{' + output.replaceAll(/(\d+)\:/ig, '"$1":') + '"end":true}');
+    // render(gamedata.video);
 
     // audio setup
     audio_manager = new AudioManager(1, 31440);  // sample rate: 262 * 228 * 60 / 114
@@ -69,15 +84,18 @@ function update() {
 }
 
 function render(output) {
+    const _dpr = dpr;
     const _pixels_image = pixels_image;
     const _ctx = ctx;
     const _output = output;
     const _colors = NTSC_colors;
-    const _width = _pixels_image.width;
-    const _height = _pixels_image.height;
+    const _width = _pixels_image.width / _dpr;
+    const _height = _pixels_image.height / _dpr;
 
     var pixels = _pixels_image.data;
     var last = 0;  // same in TIA
+
+    let woffset = 4 * _pixels_image.width;
 
     for (let y = 0; y < _height; y++) {
         for (let x = 0; x < _width; x++) {
@@ -89,18 +107,24 @@ function render(output) {
                 output_index = last;
             }
 
-            let offset = index * 4;
             let color = _colors[output_index];
 
             if (color === undefined) {
               //console.log('undefined color', index, output_index, last, _output);
               return;
             }
-            
-            pixels[offset] = color.r;
-            pixels[offset + 1] = color.g;
-            pixels[offset + 2] = color.b;
-            pixels[offset + 3] = color.a;
+
+            let xdpr = x * _dpr;
+            let ydpr = y * _dpr;
+            for (var y1 = 0; y1 < _dpr; y1++) {
+                let ydpry1 = (ydpr + y1) * woffset;
+                for (var x1 = 0; x1 < _dpr; x1++) {
+                    let offset = ((xdpr + x1) * 4) + ydpry1;
+                    pixels[offset] = color.r;
+                    pixels[offset + 1] = color.g;
+                    pixels[offset + 2] = color.b;
+                }
+            }
             last = output_index;
         }
     }
@@ -122,4 +146,16 @@ function build_complete_audio_fragment(incomplete_fragment) {
         last = incomplete_fragment[i];
     }
     return incomplete_fragment;  // return complete fragment now
+}
+
+/**
+ * instead of setting alpha every game cycle
+ * we just set it once
+ */
+function setAlpha()
+{
+    let pixels = pixels_image.data;
+    for (var i = 0, t = pixels.length; i < t; i += 4) {
+        pixels[i + 3] = 255;
+    }
 }
